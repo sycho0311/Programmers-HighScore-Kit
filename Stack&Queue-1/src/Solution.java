79aeb0204e7eff8c818a5b12cf7a70169ba78a2a
sycho <sycho0311@gmail.com>
2018-10-17 19:40:50 +0900

    Adjust Code

--- Stack&Queue-1/src/Solution.java
+++ Stack&Queue-1/src/Solution.java
@@ -3,8 +3,8 @@ import java.util.Stack;
 public class Solution {
 	
 	public int solution(String arrangement) {
-		
-		int answer = 0;
+        
+        int answer = 0;
         Stack<Integer> st = new Stack<>();
         
         for (int i = 0; i < arrangement.length(); i++) {
@@ -14,24 +14,23 @@ public class Solution {
                 st.push(i);  
             } 
         	
-        	else if (arrangement.charAt(i) == ')') {
+            else if (arrangement.charAt(i) == ')') {
                 
                 // 직전 기호가 '('인지 인덱스를 통해 판별
                 // '()' 이와 같은 모양이 형성될 경우
                 // 막대기를 자르는 작업 수행
                 // 막대기의 수를 늘린다.
-        		if (st.peek() + 1 == i) {
+                if (st.peek() + 1 == i) {
                     st.pop();
                     answer += st.size();
                 } 
                 
                 // 자르는 작업이 아닌 막대기의 끝을 확인한경우
                 // 막대기 마지막 부분을 총합에 더한다.
-        		else {
+                else {
                     st.pop();
                     answer++;
                 }
-        		
             }
         }
         
